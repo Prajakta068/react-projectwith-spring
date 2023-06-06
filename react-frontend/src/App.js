8dffa4658f26c2a644f62e9dad9d4f50132b62d9
prajakta <patilprajakta068@gmail.com>
2023-06-06 12:46:46 +0530

    third

---
+++ react-frontend/src/App.js
@@ -0,0 +1,33 @@
+//import logo from './logo.svg';
+ 
+import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import ListEmployeeComponent from './component/ListEmployeeComponent';
+import HeaderComponent from './component/HeaderComponent';
+import FooterComponent from './component/FooterComponent';
+import CreateEmployeeComponent from './component/CreateEmployeeComponent';
+import ViewEmployeeComponent from './component/ViewEmployeeComponent';
+import UpdateEmployeeComponent from './component/UpdateEmployeeComponent';
+function App() {
+  return (
+   <div>
+      <Router>
+        
+          <HeaderComponent />
+          <div className="container">
+            <Switch> 
+            <Route path = "/" exact component = {ListEmployeeComponent}></Route>
+                          <Route path = "/employees" component = {ListEmployeeComponent}></Route>
+                          <Route path = "/add-employee/:id" component = {CreateEmployeeComponent}></Route>
+                          <Route path = "/view-employee/:id" component = {ViewEmployeeComponent}></Route>
+                          {/* <Route path = "/update-employee/:id" component = {UpdateEmployeeComponent}></Route> */}
+        </Switch>
+      </div>
+      <FooterComponent />
+      
+      </Router>
+    </div>
+);
+}
+
+export default App;
