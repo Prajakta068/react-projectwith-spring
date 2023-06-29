e3873147bb3389550f0d48538a45cf2574af2535
prajakta patil <109526923+Prajakta068@users.noreply.github.com>
2023-06-29 16:39:33 +0530

    Update EmployeeService.js

--- react-frontend/src/services/EmployeeService.js
+++ react-frontend/src/services/EmployeeService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees";
-
+//EMPLOYEE_API_BASE_URL this is base reat api url
 class EmployeeService {
 
     getEmployees(){
